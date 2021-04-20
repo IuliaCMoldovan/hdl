152a4313dacbe54da40bce052eb8593ffe28431f
IMoldovan <Iulia.Moldovan@analog.com>
2021-04-20 13:54:35 +0300

    trigger_ip: Initial commit

---
+++ library/axi_trigger_ip/axi_trigger_ip.tcl
@@ -0,0 +1,22 @@
+# ip
+
+source ../scripts/adi_env.tcl
+source $ad_hdl_dir/library/scripts/adi_ip_xilinx.tcl
+
+adi_ip_create axi_trigger
+adi_ip_files axi_trigger [list \
+  "$ad_hdl_dir/library/common/up_xfer_cntrl.v" \
+  "$ad_hdl_dir/library/common/ad_rst.v" \
+  "$ad_hdl_dir/library/common/up_axi.v" \
+  "$ad_hdl_dir/library/xilinx/common/up_xfer_cntrl_constr.xdc" \
+  "$ad_hdl_dir/library/xilinx/common/ad_rst_constr.xdc" \
+  "axi_trigger.v" \
+  "probe_trigger.v" \
+  "trigger_ip_regmap.v" ]
+
+adi_ip_properties axi_trigger
+
+ipx::infer_bus_interface clk xilinx.com:signal:clock_rtl:1.0 [ipx::current_core]
+ipx::infer_bus_interface rst xilinx.com:signal:reset_rtl:1.0 [ipx::current_core]
+
+ipx::save_core [ipx::current_core]
