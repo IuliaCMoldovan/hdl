ed1fbe3e10d045aff718765f9383703421160e11
IMoldovan <Iulia.Moldovan@analog.com>
2021-06-29 12:40:23 +0300

    debug_ip: Edit system_bd. Not done

--- projects/debug_ip/zed/system_bd.tcl
+++ projects/debug_ip/zed/system_bd.tcl
@@ -7,20 +7,23 @@ set DISABLE_DMAC_DEBUG [expr !$DEBUG_BUILD]
 
 source $ad_hdl_dir/projects/common/zed/zed_system_bd.tcl
 
-create_bd_port -dir I -from  0 -to 0 debug_btn_trig
-create_bd_port -dir I -from 11 -to 0 debug_probe
-create_bd_port -dir O -from  7 -to 0 debug_led
+create_bd_port -dir I -from 0 -to 0 debug_btn_trig_ext
+create_bd_port -dir I -from 7 -to 0 debug_probe0
+create_bd_port -dir I -from 7 -to 0 debug_probe1
+create_bd_port -dir I -from 7 -to 0 debug_probe2
+create_bd_port -dir I -from 7 -to 0 debug_probe3
+create_bd_port -dir O -from 7 -to 0 debug_led
 
 # Create cells
 create_bd_cell -type ip -vlnv xilinx.com:ip:xlslice:1.0 xlslice_0
 set_property -dict [list CONFIG.DIN_TO {4} CONFIG.DIN_FROM {11} CONFIG.DIN_WIDTH {12} CONFIG.DIN_FROM {11} CONFIG.DOUT_WIDTH {8}] [get_bd_cells xlslice_0]
-connect_bd_net [get_bd_ports debug_probe] [get_bd_pins xlslice_0/Din]
-connect_bd_net [get_bd_ports debug_led] [get_bd_pins xlslice_0/Dout]
+ad_connect debug_probe0                     xlslice_0/Din
+ad_connect debug_led                        xlslice_0/Dout
 
 create_bd_cell -type ip -vlnv xilinx.com:ip:ila:6.2 ila_0
 set_property -dict [list CONFIG.C_PROBE0_WIDTH {8} CONFIG.C_DATA_DEPTH {4096} CONFIG.C_NUM_OF_PROBES {1} CONFIG.C_ENABLE_ILA_AXI_MON {false} CONFIG.C_MONITOR_TYPE {Native}] [get_bd_cells ila_0]
-connect_bd_net [get_bd_pins ila_0/probe0] [get_bd_pins xlslice_0/Dout]
-connect_bd_net [get_bd_pins ila_0/clk] [get_bd_pins sys_ps7/FCLK_CLK0]
+ad_connect ila_0/probe0 xlslice_0/Dout
+ad_connect ila_0/clk sys_ps7/FCLK_CLK0
 
 
 
@@ -42,7 +45,8 @@ ad_ip_parameter trigger_dmac CONFIG.DMA_AXI_PROTOCOL_DEST 1
 ad_ip_parameter trigger_dmac CONFIG.SYNC_TRANSFER_START true
 ad_ip_parameter trigger_dmac CONFIG.DISABLE_DEBUG_REGISTERS $DISABLE_DMAC_DEBUG
 
-ad_ip_instance proc_sys_reset trigger_reset
+ad_ip_instance proc_sys_reset sys_rstgen
+ad_ip_parameter sys_rstgen CONFIG.C_EXT_RST_WIDTH 1
 
 ad_ip_instance blk_mem_gen bram_dd
 ad_ip_parameter bram_dd CONFIG.use_bram_block {Stand_Alone}
@@ -61,76 +65,67 @@ ad_ip_parameter bram_dd CONFIG.Read_Width_B {16}
 ad_ip_parameter bram_dd CONFIG.Write_Depth_A {8192}
 
 
-# Connections
-ad_connect trigger_clk axi_trigger/clk
-ad_connect trigger_clk trigger_fifo/clk
-ad_connect trigger_clk bram_dd/clkb
-ad_connect trigger_clk bram_dd/clka
-ad_connect trigger_clk trigger_dmac/fifo_wr_clk
-ad_connect trigger_clk trigger_reset/slowest_sync_clk
-ad_connect trigger_reset/ext_reset_in sys_rstgen/peripheral_aresetn
-ad_connect trigger_reset/bus_struct_reset trigger_fifo/rst
-
-#ad_connect axi_trigger/adc_data 	trigger_fifo/data_in        
-ad_connect axi_trigger/data_valids 	trigger_fifo/data_in_valid  
-
-ad_connect trigger_fifo/addr_w 		bram_dd/addra                    
-ad_connect trigger_fifo/din_w  		bram_dd/dina                     
-ad_connect trigger_fifo/en_w   		bram_dd/ena                      
-ad_connect trigger_fifo/wea_w  		bram_dd/wea                      
-ad_connect trigger_fifo/addr_r 		bram_dd/addrb                    
-ad_connect trigger_fifo/dout_r 		bram_dd/doutb                    
-ad_connect trigger_fifo/en_r   		bram_dd/enb                      
-
-ad_connect trigger_fifo/data_out 			trigger_dmac/fifo_wr_din  
-ad_connect trigger_fifo/data_out_valid 		trigger_dmac/fifo_wr_en   
-
-ad_connect trigger_fifo/depth 				axi_trigger/fifo_depth 
-
-
-# Logic analyzer DMA
-ad_connect sys_cpu_clk trigger_dmac/m_dest_axi_aclk
-
-
-#ad_connect trigger_dmac/m_dest_axi axi_rd_wr_combiner_logic/s_wr_axi
-
-#ad_ip_parameter sys_ps7 CONFIG.PCW_USE_S_AXI_HP1 {1}
-#ad_connect sys_cpu_clk sys_ps7/S_AXI_HP1_ACLK
-#ad_connect trigger_dmac/m_dest_axi sys_ps7/S_AXI_HP1
-
-ad_connect axi_trigger/clk sys_ps7/FCLK_CLK0
-
-# Alternative for when using multi-bit valid_probes
+# connections
 create_bd_cell -type ip -vlnv xilinx.com:ip:xlconstant:1.1 xlconstant_0
 set_property -dict [list CONFIG.CONST_WIDTH {2} CONFIG.CONST_VAL {3}] [get_bd_cells xlconstant_0]
-ad_connect xlconstant_0/dout axi_trigger/valid_probes
-# # #
-#create_bd_cell -type ip -vlnv xilinx.com:ip:xlconstant:1.1 xlconstant_0
-#connect_bd_net [get_bd_pins xlconstant_0/dout] [get_bd_pins axi_trigger/data_valid_0]
-#create_bd_cell -type ip -vlnv xilinx.com:ip:xlconstant:1.1 xlconstant_1
-#connect_bd_net [get_bd_pins xlconstant_1/dout] [get_bd_pins axi_trigger/data_valid_1]
+ad_connect  xlconstant_0/dout                axi_trigger/valid_probes
+ad_connect  axi_trigger/clk                  sys_ps7/FCLK_CLK0
+ad_connect  axi_trigger/adc_data0 	         trigger_fifo/data_in        
+ad_connect  axi_trigger/data_valids 	     trigger_fifo/data_in_valid  
+ad_connect  debug_btn_trig_ext               axi_trigger/trigger_ext
+ad_connect  debug_probe0                     axi_trigger/probe0
+ad_connect  debug_probe1                     axi_trigger/probe1
+ad_connect  debug_probe2                     axi_trigger/probe2
+ad_connect  debug_probe3                     axi_trigger/probe3
+ad_connect  trigger_fifo/depth 				 axi_trigger/fifo_depth 
+ad_connect  trigger_dmac/fifo_wr_sync        axi_trigger/trigger_out
 
-#set_property -dict [list CONFIG.C_DW1 {12} CONFIG.C_NUM_OF_PROBES {3}] [get_bd_cells ila_0]
+# system reset/clock definitions
 
-# Connect nets
-# ##############ad_connect axi_trigger/trigger_out ila_0/probe2
-#ad_connect debug_probe ila_0/probe1
-ad_connect debug_probe axi_trigger/probe0
-ad_connect debug_probe axi_trigger/probe1
-ad_connect debug_probe axi_trigger/probe2
-ad_connect debug_probe axi_trigger/probe3
-ad_connect debug_btn_trig axi_trigger/trigger_ext
-ad_connect trigger_dmac/fifo_wr_sync axi_trigger/trigger_out
+ad_connect  sys_cpu_clk                      sys_ps7/FCLK_CLK0
+ad_connect  sys_cpu_reset                    sys_rstgen/peripheral_reset
+ad_connect  sys_cpu_resetn                   sys_rstgen/peripheral_aresetn
+ad_connect  sys_cpu_clk                      sys_rstgen/slowest_sync_clk
+ad_connect  sys_rstgen/ext_reset_in          sys_ps7/FCLK_RESET0_N
+
+ad_connect  trigger_clk                      trigger_fifo/clk
+ad_connect  trigger_clk                      bram_dd/clkb
+ad_connect  trigger_clk                      bram_dd/clka
+ad_connect  trigger_clk                      trigger_dmac/fifo_wr_clk
+ad_connect  trigger_clk                      sys_rstgen/slowest_sync_clk
+ad_connect  sys_rstgen/ext_reset_in          sys_rstgen/peripheral_aresetn
+ad_connect  sys_rstgen/bus_struct_reset      trigger_fifo/rst
+			         
+ad_connect  trigger_fifo/addr_w 		     bram_dd/addra                    
+ad_connect  trigger_fifo/din_w  		     bram_dd/dina                     
+ad_connect  trigger_fifo/en_w   		     bram_dd/ena                      
+ad_connect  trigger_fifo/wea_w  		     bram_dd/wea                      
+ad_connect  trigger_fifo/addr_r 		     bram_dd/addrb                    
+ad_connect  trigger_fifo/dout_r 		     bram_dd/doutb                    
+ad_connect  trigger_fifo/en_r   		     bram_dd/enb                      
+		   
+ad_connect  trigger_fifo/data_out 			trigger_dmac/fifo_wr_din  
+ad_connect  trigger_fifo/data_out_valid     trigger_dmac/fifo_wr_en   
+
+# logic analyzer DMA
+ad_connect sys_cpu_clk                      trigger_dmac/m_dest_axi_aclk
+ad_connect sys_cpu_resetn                   trigger_dmac/m_dest_axi_aresetn
 
 
-# Map
+#ad_connect trigger_dmac/m_dest_axi          axi_rd_wr_combiner_logic/s_wr_axi
+
+#ad_ip_parameter sys_ps7 CONFIG.PCW_USE_S_AXI_HP1 {1}
+#ad_connect sys_cpu_clk                      sys_ps7/S_AXI_HP1_ACLK
+#ad_connect trigger_dmac/m_dest_axi          sys_ps7/S_AXI_HP1
+
+
+# map
 ad_cpu_interconnect 0x44000000 axi_trigger
 ad_cpu_interconnect 0x7C400000 trigger_dmac
 
-ad_mem_hp1_interconnect $sys_cpu_clk sys_ps7/S_AXI_HP1
-ad_mem_hp1_interconnect $sys_cpu_clk trigger_dmac/m_dest_axi
+ad_mem_hp1_interconnect $sys_cpu_clk        sys_ps7/S_AXI_HP1
+ad_mem_hp1_interconnect $sys_cpu_clk        trigger_dmac/m_dest_axi
 
-ad_connect  sys_cpu_resetn trigger_dmac/m_dest_axi_aresetn
 
-# Interrupts
+# interrupts
 ad_cpu_interrupt ps-10 mb-11 trigger_dmac/irq
